3d7956d659f098d2f84546a321763565b64f6824
kartik <kabakshi@hotmail.ca>
2021-11-16 19:17:18 -0500

    updating the counter on the page to change with input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,23 @@
 // This file will be solely responsible for this character counter
 
-// Add a $(document).ready() function to your file to ensure the DOM has loaded
-
-
-// console.log('hello')
 $(document).ready(function() {
   // --- our code goes here ---
-  // console.log('ready')
-  document.addEventListener("click", (event) => {
-    console.log(event);
-     
+  $('textarea').keyup(function() {
+    
     let count = $(this).val().length;
-  })
 
+    if (count <= 140) {
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass("negative-count")
+        .text(140 - count);
+    } else {
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .addClass("negative-count")
+        .text(140 - count);
+    }
+  })
 })
